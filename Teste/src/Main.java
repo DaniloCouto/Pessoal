f26343e76329e10ad6b6a5399839b58cefeede80
Danilo Soares Couto <danilo.s.couto@gmail.com>
2015-05-06 11:04:24 -0300

    Fase Inicial do Projeto Teste

---
+++ Teste/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Teste t = new Teste(1,"Oi", 3.5);
+		System.out.println("Id :"+t.getI()+"\nNome :"+t.getS()+"\nValor :"+t.getV());
+	}
+
+}
---
+++ Teste/src/Teste.java
@@ -0,0 +1,32 @@
+
+public class Teste {
+	int i;
+	String s;
+	double v;
+	
+	public Teste(int j, String string, double d) {
+		i = j;
+		s = string;
+		v = d;
+	}
+	public double getV() {
+		return v;
+	}
+	public void setV(double v) {
+		this.v = v;
+	}
+	public int getI() {
+		return i;
+	}
+	public void setI(int i) {
+		this.i = i;
+	}
+	public String getS() {
+		return s;
+	}
+	public void setS(String s) {
+		this.s = s;
+	}
+	
+	
+}
